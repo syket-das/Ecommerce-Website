2f4bb13e2d87d1efe0c9bfbfd36431d107abe7cd
syket-das <saiketsd23@gmail.com>
2022-01-15 23:46:16 +0530

    header disigned

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
-  return <>Hello world</>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <h1>Hello world</h1>
+        </main>
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
