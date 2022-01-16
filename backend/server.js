e0ff60a5bff70249c4f88ef797e261a5730101f4
syket-das <saiketsd23@gmail.com>
2022-01-16 14:17:44 +0530

    product routes done

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 
-import products from './data/products.js';
 import connectDB from './config/db.js';
+
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -14,14 +15,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
 
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
