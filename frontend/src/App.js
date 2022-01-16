6b94c5e76096837977d03ea6dbcc951dfe42ecf1
syket-das <saiketsd23@gmail.com>
2022-01-16 11:28:11 +0530

    product screen done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
+
       <Container>
         <main className="py-3">
-          <h1>Hello world</h1>
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </main>
       </Container>
       <Footer />
-    </>
+    </Router>
   );
 }
 
