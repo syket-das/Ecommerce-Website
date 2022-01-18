0abf29675b8b31f33d49bb91523cd5f44d3e72ac
syket-das <saiketsd23@gmail.com>
2022-01-19 01:25:07 +0530

    user route and controller added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 // middlewares
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -14,12 +15,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users/login', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
