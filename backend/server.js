ca13ec99166bd3b5e8e91d15e84bf2ab6db3d63e
syket-das <saiketsd23@gmail.com>
2022-01-20 00:31:25 +0530

    jwt prive route done

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
-app.use('/api/users/login', userRoutes);
+app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
